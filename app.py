4c84a8f902ccd12565edbae098ae114bcb8ccdcd
Nicholas Chan <nicholasch.an@outlook.com>
2023-08-31 15:09:03 -0700

    asdf

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 from flask import Flask, redirect, render_template, request
 
-from cs50 import sql
-
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    render_template("index.html")
+    return render_template("index.html")
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/edit", methods=["POST","GET"])
 def edit():
-    
     if request.method == "POST":
         return redirect("/edit")
-    
     else:
-        return render_template("edit.html", 400)
+        return render_template("edit.html")
+    
