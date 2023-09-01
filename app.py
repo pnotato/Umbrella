5b0470fbc6dbbcc08115ae0008490c431bb03336
Nicholas Chan <nicholasch.an@outlook.com>
2023-08-31 18:57:33 -0700

    new changes

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, redirect, render_template, request
+from flask_session import Session
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/")
 def index():
